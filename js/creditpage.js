2d1c831fbce975b629a43cf5aa808e90ae383caa
Nohahossam10 <nohahossam38@gmail.com>
2019-12-08 18:45:25 +0200

    adding credit page to go credits page

---
+++ js/creditpage.js
@@ -0,0 +1,8 @@
+let credit =document.getElementById("made_by");
+function creditsPage()
+{
+    
+    window.location.href="credits.html","_self" ;
+}
+
+credit.addEventListener('click',creditsPage);
